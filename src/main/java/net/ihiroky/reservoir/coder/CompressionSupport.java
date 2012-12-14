46ae7673fa5e80d0701ac2978994cfad0cded4ff
Hiroki Itoh <ihiroky@gmail.com>
2012-12-14 09:42:55 +0900

    Fix compression resource leak and synchronization
    DeflaterOutputStream(OutputStream, Deflater) and doesn't close the compressor.
    To close the compressor, create sub class of DeflaterOutputStream and call
    def.end() in its close().
    
    Deflater and Inflater is not thread safe. Create these instances per a method
    invocation. I don't synchronize a field from the standpoint of concurrency,

--- src/main/java/net/ihiroky/reservoir/coder/CompressionSupport.java
+++ src/main/java/net/ihiroky/reservoir/coder/CompressionSupport.java
@@ -3,6 +3,7 @@ package net.ihiroky.reservoir.coder;
 import net.ihiroky.reservoir.Coder;
 import net.ihiroky.reservoir.PropertiesSupport;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
@@ -35,12 +36,37 @@ public class CompressionSupport {
         return enabled;
     }
 
+    /**
+     * Has a constructor that sets a compression level.
+     */
+    static class LevelDeflaterOutputStream extends DeflaterOutputStream {
+
+        /**
+         * Creates LevelDeflaterOutputStream.
+         * @param outputStream base OutputStream.
+         * @param level compression level.
+         */
+        LevelDeflaterOutputStream(OutputStream outputStream, int level) {
+            super(outputStream, new Deflater(level));
+        }
+
+        /**
+         * Closes this stream and deflater used by this stream..
+         * @throws IOException
+         */
+        @Override
+        public void close() throws IOException {
+            super.close();
+            def.end();
+        }
+    }
+
     public OutputStream createOutputStreamIfEnabled(OutputStream outputStream) {
-        return enabled ? new DeflaterOutputStream(outputStream, new Deflater(level)) : outputStream;
+        return enabled ? new LevelDeflaterOutputStream(outputStream, level) : outputStream;
     }
 
     public InputStream createInputStreamIfEnabled(InputStream inputStream) {
-        return enabled ? new InflaterInputStream(inputStream, new Inflater()) : inputStream;
+        return enabled ? new InflaterInputStream(inputStream) : inputStream;
     }
 
     public <V> Coder.Encoder<V> createEncoderIfEnabled(Coder.Encoder<V> encoder) {
@@ -59,46 +85,44 @@ public class CompressionSupport {
 
     static class DeflateEncoder<V> implements Coder.Encoder<V> {
 
-        private Coder.Encoder<V> encoder;
-        private Deflater deflater;
+        private final Coder.Encoder<V> encoder;
+        private final int level;
 
         DeflateEncoder(Coder.Encoder<V> encoder, int level) {
             this.encoder = encoder;
-            this.deflater = new Deflater(level);
+            this.level = level;
         }
 
         @Override
         public ByteBuffer encode(V value) {
             ByteBuffer encoded = encoder.encode(value);
-            if (!encoded.hasArray()) {
-                // TODO
-                throw new UnsupportedOperationException("no implemented yet.");
+            if (!encoded.hasRemaining()) {
+                return encoded;
             }
+
             int inputSize = encoded.remaining();
-            Deflater deflater = this.deflater;
-            deflater.setInput(encoded.array(), encoded.arrayOffset(), inputSize);
-            deflater.finish();
             byte[] buffer = new byte[inputSize / 2 + 2]; // + 2 : keep ge 2 for expend()
             int deflated = 0;
+            Deflater deflater = new Deflater(level);
+            deflater.setInput(encoded.array(), encoded.arrayOffset(), inputSize);
+            deflater.finish();
             while (!deflater.finished()) {
                 if (deflated == buffer.length) {
                     buffer = expand(buffer);
                 }
                 deflated += deflater.deflate(buffer, deflated, buffer.length - deflated);
             }
-            deflater.reset();
+            deflater.end();
             return ByteBuffer.wrap(buffer, 0, deflated);
         }
     }
 
     static class InflateDecoder<V> implements Coder.Decoder<V> {
 
-        private Coder.Decoder<V> decoder;
-        private Inflater inflater;
+        private final Coder.Decoder<V> decoder;
 
         InflateDecoder(Coder.Decoder<V> decoder) {
             this.decoder = decoder;
-            this.inflater = new Inflater();
         }
 
         @Override
@@ -108,10 +132,10 @@ public class CompressionSupport {
                 throw new UnsupportedOperationException("no implemented yet.");
             }
             int inputSize = byteBuffer.remaining();
-            Inflater inflater = this.inflater;
-            inflater.setInput(byteBuffer.array(), byteBuffer.arrayOffset(), inputSize);
             byte[] buffer = new byte[inputSize * 2 + 2];
             int inflated = 0;
+            Inflater inflater = new Inflater();
+            inflater.setInput(byteBuffer.array(), byteBuffer.arrayOffset(), inputSize);
             try {
                 while (!inflater.finished()) {
                     if (inflated == buffer.length) {
@@ -121,8 +145,10 @@ public class CompressionSupport {
                 }
             } catch (DataFormatException e) {
                 throw new RuntimeException("failed to decode.", e);
+            } finally {
+                inflater.end();
             }
-            inflater.reset();
+
             return decoder.decode(ByteBuffer.wrap(buffer, 0, inflated));
         }
     }
